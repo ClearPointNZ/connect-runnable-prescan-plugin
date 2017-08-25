67f9d25d8afa585659a63d6fbc1cde2309cfe7a5
Karl Leaning <karl.leaning@objectivity.co.nz>
2017-08-25 15:13:47 +1200

    remove unused imports

--- src/main/java/cd/connect/prescan/PrescanGeneratorMojo.java
+++ src/main/java/cd/connect/prescan/PrescanGeneratorMojo.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Mojo(name = "prescan-config",
